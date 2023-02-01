c396af13a6ca861f90ecc4a00cef3c6274b19733
southdog0311 <75650097+southdog0311@users.noreply.github.com>
2023-02-01 10:56:55 +0800

    Add files via upload
    add-file

---
+++ 網路程式設計期末/網路程式設計期末/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace GuessNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Label7 = new System.Windows.Forms.Label();
+            this.TextBox5 = new System.Windows.Forms.TextBox();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.ListBox2 = new System.Windows.Forms.ListBox();
+            this.ListBox1 = new System.Windows.Forms.ListBox();
+            this.TextBox4 = new System.Windows.Forms.TextBox();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.TextBox3 = new System.Windows.Forms.TextBox();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(285, 115);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(75, 23);
+            this.Button2.TabIndex = 86;
+            this.Button2.Text = "送出";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Label7
+            // 
+            this.Label7.AutoSize = true;
+            this.Label7.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Label7.Location = new System.Drawing.Point(216, 99);
+            this.Label7.Name = "Label7";
+            this.Label7.Size = new System.Drawing.Size(98, 13);
+            this.Label7.TabIndex = 85;
+            this.Label7.Text = "猜測對手的數字";
+            this.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // TextBox5
+            // 
+            this.TextBox5.Location = new System.Drawing.Point(214, 115);
+            this.TextBox5.Name = "TextBox5";
+            this.TextBox5.Size = new System.Drawing.Size(65, 22);
+            this.TextBox5.TabIndex = 84;
+            this.TextBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Label6.Location = new System.Drawing.Point(226, 146);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(85, 13);
+            this.Label6.TabIndex = 83;
+            this.Label6.Text = "我的猜測紀錄";
+            this.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Label5.Location = new System.Drawing.Point(33, 146);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(85, 13);
+            this.Label5.TabIndex = 82;
+            this.Label5.Text = "對手猜測紀錄";
+            this.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // ListBox2
+            // 
+            this.ListBox2.FormattingEnabled = true;
+            this.ListBox2.ItemHeight = 12;
+            this.ListBox2.Location = new System.Drawing.Point(209, 162);
+            this.ListBox2.Name = "ListBox2";
+            this.ListBox2.Size = new System.Drawing.Size(120, 172);
+            this.ListBox2.TabIndex = 81;
+            // 
+            // ListBox1
+            // 
+            this.ListBox1.FormattingEnabled = true;
+            this.ListBox1.ItemHeight = 12;
+            this.ListBox1.Location = new System.Drawing.Point(26, 162);
+            this.ListBox1.Name = "ListBox1";
+            this.ListBox1.Size = new System.Drawing.Size(120, 172);
+            this.ListBox1.TabIndex = 80;
+            // 
+            // TextBox4
+            // 
+            this.TextBox4.Location = new System.Drawing.Point(27, 115);
+            this.TextBox4.Name = "TextBox4";
+            this.TextBox4.Size = new System.Drawing.Size(64, 22);
+            this.TextBox4.TabIndex = 79;
+            this.TextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Label3.Location = new System.Drawing.Point(24, 99);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(132, 13);
+            this.Label3.TabIndex = 78;
+            this.Label3.Text = "我的數字(四位不重複)";
+            this.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Label4.Location = new System.Drawing.Point(14, 19);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(53, 13);
+            this.Label4.TabIndex = 77;
+            this.Label4.Text = "接聽Port";
+            this.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.TextBox1.Location = new System.Drawing.Point(73, 14);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(73, 23);
+            this.TextBox1.TabIndex = 76;
+            this.TextBox1.Text = "2013";
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(158, 14);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(75, 23);
+            this.Button1.TabIndex = 75;
+            this.Button1.Text = "啟動接聽";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Label2.Location = new System.Drawing.Point(170, 57);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(53, 13);
+            this.Label2.TabIndex = 74;
+            this.Label2.Text = "目標Port";
+            this.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Label1.Location = new System.Drawing.Point(14, 57);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(44, 13);
+            this.Label1.TabIndex = 73;
+            this.Label1.Text = "目標IP";
+            this.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // TextBox3
+            // 
+            this.TextBox3.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.TextBox3.Location = new System.Drawing.Point(229, 52);
+            this.TextBox3.Name = "TextBox3";
+            this.TextBox3.Size = new System.Drawing.Size(65, 23);
+            this.TextBox3.TabIndex = 72;
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.TextBox2.Location = new System.Drawing.Point(63, 52);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(88, 23);
+            this.TextBox2.TabIndex = 71;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(375, 348);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.Label7);
+            this.Controls.Add(this.TextBox5);
+            this.Controls.Add(this.Label6);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.ListBox2);
+            this.Controls.Add(this.ListBox1);
+            this.Controls.Add(this.TextBox4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.TextBox3);
+            this.Controls.Add(this.TextBox2);
+            this.Name = "Form1";
+            this.Text = "猜數字遊戲";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button Button2;
+        internal System.Windows.Forms.Label Label7;
+        internal System.Windows.Forms.TextBox TextBox5;
+        internal System.Windows.Forms.Label Label6;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.ListBox ListBox2;
+        internal System.Windows.Forms.ListBox ListBox1;
+        internal System.Windows.Forms.TextBox TextBox4;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.TextBox TextBox1;
+        internal System.Windows.Forms.Button Button1;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.TextBox TextBox3;
+        internal System.Windows.Forms.TextBox TextBox2;
+    }
+}
+
---
+++ 網路程式設計期末/網路程式設計期末/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;//匯入網路通訊協定相關函數
+using System.Net.Sockets;//匯入網路插座功能函數
+using System.Threading;//匯入多執行緒功能函數
+
+namespace GuessNumber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        UdpClient U;//宣告UDP通訊物件
+        Thread Th;//宣告監聽用執行緒
+        //啟動監聽按鍵程序
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false; //忽略跨執行緒操作的錯誤
+            Th = new Thread(Listen); //建立監聽執行緒，目標副程序→Listen
+            Th.Start(); //啟動監聽執行緒
+            Button1.Enabled = false; //按鍵失效，不能(也不需要)重複開啟監聽
+        }
+        //監聽副程序
+        private void Listen()
+        {
+            int Port = int.Parse(TextBox1.Text); //設定監聽用的通訊埠
+            U = new UdpClient(Port); //建立UDP監聽器
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), Port); //建立本機端點資訊
+            while (true) //持續監聽的無限迴圈→有訊息(True)就處理，無訊息就等待！
+            {
+                byte[] B = U.Receive(ref EP);//訊息到達時讀取資訊到B陣列
+                string R = Encoding.Default.GetString(B);//翻譯B陣列為字串R
+                if (R.Substring(1, 1) == "A")//檢視第二字元(是否為"A")
+                {//收到對手回應我的猜測提示("?A?B")
+                    ListBox2.Items.Add(TextBox5.Text + ">" + R);//紀錄猜測提示
+                    Button2.Enabled = false;//暫停猜數字按鍵功能，輪到對手猜數字
+                    if (R == "4A0B") { MessageBox.Show("你贏了！"); }//全部猜對，顯示我方勝
+                }
+                else
+                {//收到對手猜測我的數字
+                    string K = chk(R);//檢測數字對位的狀況，產生提示字串K
+                    Send(K);//用網路送出提示
+                    ListBox1.Items.Add(R + ">" + K);//紀錄對手猜過的數字
+                    Button2.Enabled = true;//啟動按鍵功能，輪到我猜了！
+                    if (K == "4A0B") { MessageBox.Show("你輸了！"); }//對手全部猜對，顯示我方敗
+                }
+            }
+        }
+        //找出本機IP
+        private string MyIP()
+        {
+            string hn = Dns.GetHostName();//取得本機電腦名稱
+            IPAddress[] ip = Dns.GetHostEntry(hn).AddressList; //取得本機IP陣列(可能有多個)
+            foreach (IPAddress it in ip)//列舉各個IP
+            {
+                if (it.AddressFamily == AddressFamily.InterNetwork)//如果是IPv4格式
+                {
+                    return it.ToString();//回傳此IP字串
+                }
+            }
+            return ""; //找不到合格IP，回傳空字串
+        }
+        //表單載入
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text += " " + MyIP();//顯示本機IP於標題列
+        }
+        //關閉接聽執行續(如果有的話)
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Th.Abort(); //關閉監聽執行緒
+                U.Close(); //關閉監聽器
+            }
+            catch
+            {
+                //如果監聽執行緒未建立會出現錯誤，程式跳到此處執行，
+                //此處不寫程式就是忽略錯誤，程式將繼續執行！
+            }
+        }
+
+        //傳送字串給對手
+        private void Send(string A)
+        {
+            int Port = int.Parse(TextBox3.Text);//對手通訊埠
+            UdpClient S = new UdpClient(TextBox2.Text, Port);//宣告UDP通訊物件
+            byte[] B = Encoding.Default.GetBytes(A);//轉譯字串為Byte陣列
+            S.Send(B, B.Length);//送出資訊
+            S.Close();//關閉通訊物件
+        }
+        //檢測對位狀況回傳提示字串
+        private string chk(string G)
+        {
+            int A = 0, B = 0;//數字對位變數
+            char[] C = TextBox4.Text.ToCharArray();//我的密碼的字元陣列
+            char[] D = G.ToCharArray();//猜測數字的字元陣列
+            //C與D陣列元素交叉比對
+            for (int i = 0; i <= 3; i++)
+            {
+                for (int j = 0; j <= 3; j++)
+                {
+                    if (C[i] == D[j])//字元正確
+                    {
+                        if (i == j)//位置正確
+                        {
+                            A += 1;//A加一
+                        }
+                        else//位置不正確
+                        {
+                            B += 1;//B加一
+                        }
+                    }
+                }
+            }
+            return A.ToString() + "A" + B.ToString() + "B";//回傳"?A?B"字串
+        }
+        //送出猜測數字
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            int rv;//宣告一整數
+            bool isnum = int.TryParse(TextBox5.Text, out rv);//可否翻譯為整數？
+            if (TextBox5.Text.Length != 4 || isnum == false)
+            {
+                MessageBox.Show("必須是四個數字！");
+                return;//跳出副程序
+            }
+            char[] C = TextBox5.Text.ToCharArray();//將你猜的數字轉成字元陣列
+            bool rpt = false;//數字是否重複的變數
+            //交叉比對C陣列各元素
+            for (int i = 0; i <= 3; i++)
+            {
+                for (int j = 0; j <= 3; j++)
+                {
+                    if (C[i] == C[j] && i != j)//字元相同
+                    {
+                        rpt = true;//有重複
+                    }
+                }
+            }
+            if (rpt)//如果數字重複
+            {
+                MessageBox.Show("數字不能重複！");
+                return;//跳出副程序
+            }
+            Send(TextBox5.Text);//用網路送出字串
+        }
+    }
+}
